a0fb628c25265387cf313b890525210a13be400e
QuintinPope <60300243+QuintinPope@users.noreply.github.com>
2023-07-11 12:05:06 -0700

    Update get_data.py
    Added ANLI support, restructured file to be more modular.

--- model_training/get_data.py
+++ model_training/get_data.py
@@ -4,7 +4,14 @@ from transformers import AutoTokenizer
 from datasets import load_dataset
 import pandas as pd
 
-def get_mnli_dataset(model_name = "bert-base-uncased", longest_sequence_allowed = 64, n_test_data = 10000, make_binary = True, data_path = "data/mnli", device = "cuda:0", save_data = True):
+def get_mnli_dataset(model_name = "bert-base-uncased", 
+                     longest_sequence_allowed = 64, 
+                     n_test_data = 10000, 
+                     make_binary = True, 
+                     data_path = "data/mnli", 
+                     device = "cuda:0", 
+                     save_data = True,
+                     data_label = "mnli"):
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     mnli_dataset = load_dataset("multi_nli")
     mnli_pandas = mnli_dataset['train'].to_pandas()[["premise", "hypothesis", "label"]]
@@ -12,12 +19,92 @@ def get_mnli_dataset(model_name = "bert-base-uncased", longest_sequence_allowed 
     mnli_pandas = mnli_pandas.append(mnli_dataset['validation_mismatched'].to_pandas()[["premise", "hypothesis", "label"]])
     mnli_pandas = mnli_pandas.sample(frac = 1, random_state=42)
 
-    current_tokens = tokenize_data(mnli_pandas, tokenizer, max_length = longest_sequence_allowed + 10)
-    inputs_too_long_flag = torch.sum(current_tokens > 0, dim = 1) <= longest_sequence_allowed
-    inputs_short_enough_entries = [i for i in range(len(mnli_pandas)) if inputs_too_long_flag[i]]
+    train_data, train_labels, test_data, test_labels = filter_data(mnli_pandas, 
+                                                                   tokenizer,
+                                                                   longest_sequence_allowed,
+                                                                   make_binary,
+                                                                   n_test_data,
+                                                                   device)
+    if save_data:
+        save_data_to_file(train_data, 
+                          train_labels, 
+                          test_data, 
+                          test_labels, 
+                          data_path,
+                          data_label)
+    return train_data, train_labels, test_data, test_labels
 
-    short_mnli_data = mnli_pandas.iloc[inputs_short_enough_entries]
-    short_mnli_labels = torch.from_numpy(mnli_pandas.iloc[inputs_short_enough_entries]["label"].values)
+def get_anli_dataset(model_name = "bert-base-uncased", 
+                     longest_sequence_allowed = 64, 
+                     n_test_data = 10000, 
+                     make_binary = True, 
+                     data_path = "data/anli", 
+                     device = "cuda:0", 
+                     save_data = True,
+                     data_label = "anli"):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    anli_dataset = load_dataset("anli")
+    anli_pandas = anli_dataset['train_r3'].to_pandas()[["premise", "hypothesis", "label"]]
+    anli_pandas = anli_pandas.append(anli_dataset['test_r3'].to_pandas()[["premise", "hypothesis", "label"]])
+    anli_pandas = anli_pandas.append(anli_dataset['dev_r3'].to_pandas()[["premise", "hypothesis", "label"]])
+    anli_pandas = anli_pandas.sample(frac = 1, random_state=42)
+
+    train_data, train_labels, test_data, test_labels = filter_data(anli_pandas, 
+                                                                   tokenizer,
+                                                                   longest_sequence_allowed,
+                                                                   make_binary,
+                                                                   n_test_data,
+                                                                   device)
+    if save_data:
+        save_data_to_file(train_data, 
+                          train_labels, 
+                          test_data, 
+                          test_labels, 
+                          data_path,
+                          data_label)
+    return train_data, train_labels, test_data, test_labels
+
+def save_data_to_file(train_data, 
+                      train_labels, 
+                      test_data, 
+                      test_labels, 
+                      path,
+                      dataset_label):
+    data_save_relative_path = "../" + path + "/" + dataset_label
+    print("Attempting to save data to relative path: \"" + data_save_relative_path + "_[stuff].pth" + "\"")
+    torch.save(train_data, data_save_relative_path + "_training_data.pth")
+    torch.save(train_labels, data_save_relative_path + "_training_labels.pth")
+    torch.save(test_data, data_save_relative_path + "_testing_data.pth")
+    torch.save(test_labels, data_save_relative_path + "_testing_labels.pth")
+
+def load_data_from_file(path, dataset_label):
+    data_load_relative_path = "../" + path + "/" + dataset_label
+    print("Attempting to load data from relative path: \"" + data_load_relative_path + "_[stuff].pth" + "\"")
+    train_data = torch.load(data_load_relative_path + "_training_data.pth")
+    train_labels = torch.load(data_load_relative_path + "_training_labels.pth")
+    test_data = torch.load(data_load_relative_path + "_testing_data.pth")
+    test_labels = torch.load(data_load_relative_path + "_testing_labels.pth")
+    return train_data, train_labels, test_data, test_labels
+
+def tokenize_data(data, tokenizer, max_length = 128):
+    text_data = []
+    for i in range(len(data)):
+        current_text = data.iloc[i]["premise"] + " [SEP] " + data.iloc[i]["hypothesis"]
+        text_data.append(current_text)
+    return torch.tensor(tokenizer.batch_encode_plus(text_data, padding = "longest", max_length = max_length, truncation = True)['input_ids'])
+
+def filter_data(data, 
+                tokenizer,
+                longest_sequence_allowed = 64,
+                make_binary = True,
+                n_test_data = 10000,
+                device = "cpu"):
+    current_tokens = tokenize_data(data, tokenizer, max_length = longest_sequence_allowed + 10)
+    inputs_too_long_flag = torch.sum(current_tokens > 0, dim = 1) <= longest_sequence_allowed
+    inputs_short_enough_entries = [i for i in range(len(data)) if inputs_too_long_flag[i]]
+
+    short_mnli_data = data.iloc[inputs_short_enough_entries]
+    short_mnli_labels = torch.from_numpy(data.iloc[inputs_short_enough_entries]["label"].values)
     if make_binary:
         binary_labels_selector_flag = short_mnli_labels != 1
         inputs_only_entailment_or_contradiction_entries = [i for i in range(len(short_mnli_labels)) if binary_labels_selector_flag[i]]
@@ -36,21 +123,4 @@ def get_mnli_dataset(model_name = "bert-base-uncased", longest_sequence_allowed 
     train_labels = selected_labels[train_set_selection_indices].to(device)
     test_data = selected_tokens[test_set_selection_indices].to(device)
     test_labels = selected_labels[test_set_selection_indices].to(device)
-
-    if save_data:
-        torch.save(train_data, "../" + data_path + "/mnli_training_data.pth")
-        torch.save(train_labels, "../" + data_path + "/mnli_training_labels.pth")
-        torch.save(test_data, "../" + data_path + "/mnli_testing_data.pth")
-        torch.save(test_labels, "../" + data_path + "/mnli_testing_labels.pth")
-
     return train_data, train_labels, test_data, test_labels
-
-
-
-def tokenize_data(data, tokenizer, max_length = 128):
-    text_data = []
-    for i in range(len(data)):
-        current_text = data.iloc[i]["premise"] + " [SEP] " + data.iloc[i]["hypothesis"]
-        text_data.append(current_text)
-
-    return torch.tensor(tokenizer.batch_encode_plus(text_data, padding = "longest", max_length = max_length, truncation = True)['input_ids'])
